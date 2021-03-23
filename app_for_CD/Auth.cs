91430670c73f9b471b407980f48543c035f392b5
vova <vovlo97@gmail.com>
2021-03-23 08:54:09 +0500

    Add project files.

---
+++ app_for_CD/Auth.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using Oracle.DataAccess.Client;
+
+namespace app_for_CD
+{
+    public partial class Auth : Form
+    {
+        public Auth()
+        {
+            InitializeComponent();
+            SetConnection();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            string name = textBox1.Text;
+            string pass = textBox2.Text;
+            OracleCommand cmd = con.CreateCommand();
+            cmd.Parameters.Add(new OracleParameter("LOGIN", name));
+            cmd.Parameters.Add(new OracleParameter("PASSW", pass));
+            cmd.CommandText = "select * from users_cd where login = :LOGIN and PASSWORD = :PASSW";
+            cmd.CommandType = CommandType.Text;
+            OracleDataReader dr = cmd.ExecuteReader();
+            if (dr.HasRows)
+            {
+                CloseConnection();
+                this.Close();
+            }
+            else
+            {
+                MessageBox.Show("Неправильное имя или пароль");
+            }
+
+        }
+
+
+
+        OracleConnection con = null;
+        private void SetConnection()
+        {
+            string ConnectionString = "USER ID=GGUZDR_APP;PASSWORD=gguzdr_app;DATA SOURCE=10.1.50.12:1521/GDBDRCT1";
+            con = new OracleConnection(ConnectionString);
+            try
+            {
+                con.Open();
+            }
+
+            catch (Exception e)
+            {
+                string errorMessage = "Error: ";
+                errorMessage = String.Concat(errorMessage, e.Message);
+            }
+        }
+        private void CloseConnection()
+        {
+            con.Close();
+        }
+
+    }
+}
---
+++ app_for_CD/Form2.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace app_for_CD
+{
+
+    public partial class filter : Form
+    {
+        public filter()
+        {
+            InitializeComponent();
+        }
+        private void CRM_CheckedChanged(object sender, EventArgs e)
+        {
+
+            CheckBox checkBox = (CheckBox)sender;
+            if (checkBox.Checked == true)
+            {
+                Data.f_c = 1;
+            }
+            else
+            {
+                Data.f_c = 0;
+            }
+        }
+
+        private void Серия_CheckedChanged(object sender, EventArgs e)
+        {
+            CheckBox checkBox = (CheckBox)sender;
+            if (checkBox.Checked == true)
+            {
+                Data.f_s = 1;
+            }
+            else
+            {
+                Data.f_s = 0;
+            }
+        }
+        string change_format_dateTime(string dt)
+        {
+            string tmp;
+            tmp = "20" + dt[9] + dt[10] + dt[6] + dt[7] + dt[3] + dt[4];
+            return tmp;
+        }
+
+        private void checkBox2_CheckedChanged(object sender, EventArgs e)
+        {
+            Data.st_data_orig = change_format_dateTime(dateTimePicker1.Value.ToString());
+            Data.end_data_orig = change_format_dateTime(dateTimePicker2.Value.ToString());
+            CheckBox checkBox = (CheckBox)sender;
+            if (checkBox.Checked == true)
+            {
+                Data.f_d = 1;
+            }
+            else
+            {
+                Data.f_d = 0;
+            }
+        }
+
+        private void Ok_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+
+    }
+}
